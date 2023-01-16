c66b49bd819c22e21f9e24393dfdeebc8198901d
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-01-15 20:53:53 -0500

    define post/dramas/new and map to ctrl action

--- routes/dramas.js
+++ routes/dramas.js
@@ -3,11 +3,14 @@ import * as dramasCtrl from '../controllers/dramas.js'
 
 const router = Router()
 
-// GET/ DRAMAS
+// GET/DRAMAS
 router.get('/', dramasCtrl.index)
 
 //GET/DRAMAS/NEW
 router.get('/new', dramasCtrl.new)
+
+//POST/DRAMAS/NEW
+router.post('/new', dramasCtrl.create)
 
 export {
   router
